f0e34978f0c42c1747a0340d7cde8dfaf16d2a54
Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2023-08-31 15:54:23 +0700

    26. Remove Duplicates from Sorted Array

--- 26_Remove_Duplicates_from_Sorted_Array.js
+++ 26_Remove_Duplicates_from_Sorted_Array.js
@@ -1,3 +1,18 @@
 var removeDuplicates = function(nums) {
-    
-};
+    let idx = 1
+       
+    for (let i = 1; i < nums.length; i++){
+        if (nums[i] != nums[i-1]){
+            nums[idx] = nums[i]
+            idx++
+        }
+    }
+    return idx
+}
+
+// TIME COMPLEXITY O(N)
+nums = [1,1,2]
+console.log(removeDuplicates(nums))
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+console.log(removeDuplicates(nums))
