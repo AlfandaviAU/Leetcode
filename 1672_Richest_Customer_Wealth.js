3e785758c8e12e3459f67dac94fbe089b7d01d5c
Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2023-08-31 15:32:41 +0700

    first commit

---
+++ 1672_Richest_Customer_Wealth.js
@@ -0,0 +1,21 @@
+var maximumWealth = function(accounts) {
+    let max = 0
+    
+    for (let i = 0; i < accounts.length; i++){
+        let temp = 0
+        for (let j = 0; j < accounts[i].length; j++){
+            temp += accounts[i][j]
+        }
+        if (temp > max){
+            max = temp
+        }
+    }
+    return max
+};
+
+
+// TIME COMPLEXITY O(N^2)
+
+accounts = [[1,2,3],[3,2,1]]
+
+console.log(maximumWealth(accounts))
